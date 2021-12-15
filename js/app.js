d3c0f4790a51e038996cc0b75307c4e3940431fb
Đặng Quang <quangcony.01@gmail.com>
2021-12-15 15:09:53 +0700

    update html & css

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@ const users = [
     {
         name: "Ngọc Quang",
         password: "13102001",
-        birthday: "13 Oct 2001",
+        birthday: "2001/10/13",
+        birthday_En: "13 Oct 2001",
         paths: ['./images/1 (2).jpg', './images/1 (1).jpg',
          './images/1 (3).jpg', './images/1 (4).jpg'],
         bd_greetings: "lời chúc mừng sinh nhật",
@@ -21,7 +22,8 @@ const users = [
     {
         name: "Kiều Linh",
         password: "17122001",
-        birthday: "17 Dec 2001",
+        birthday: "2001/12/17",
+        birthday_En: "17 Dec 2001",
         paths: ['https://kenh14cdn.com/thumb_w/660/203336854389633024/2021/12/8/v-bts-1-1638898969121515788442.jpg', 'https://scontent.fdad3-2.fna.fbcdn.net/v/t1.18169-9/c0.53.480.480a/s851x315/19260501_125463468037230_6485443371079209673_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=da31f3&_nc_ohc=soYF5zU9tyQAX_SUzlJ&tn=A-vkNrbUt3hg3VdY&_nc_ht=scontent.fdad3-2.fna&oh=00_AT-AZWXdoYr_d_rhCHkWdj_bFtUrzbPSkUDF8vzKaUUCwA&oe=61DF225D',
          'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1603095343/uj1ekhsjb7nqvfhfmzho.jpg', 'https://media-cdn.laodong.vn/Storage/NewsPortal/2020/8/21/829816/Ji-Chang-Wook.jpg'],
         bd_greetings: "lời chúc mừng sinh nhật",
@@ -34,8 +36,12 @@ const startParty = () => {
 }
 
 loadInfo = (user) => {
+
+    // get age to logo
+    document.querySelector('#age').textContent = getAge(user.birthday)
+
     document.querySelector('#info-name').textContent = user.name
-    document.querySelector('#info-birthday').textContent = user.birthday
+    document.querySelector('#info-birthday').textContent = user.birthday_En
     document.querySelector('#birthday-greetings').textContent = user.bd_greetings
     document.querySelector('#collection').innerHTML = ''
 
@@ -49,6 +55,17 @@ loadInfo = (user) => {
 
     })
 
+}
+
+getAge = (date) => {
+    var today = new Date();
+    var birthDate = new Date(date);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
 
 // button event
@@ -88,4 +105,5 @@ document.querySelector('#btn-loggon').addEventListener('click', () => {
 document.querySelector('#darkmode-switch').addEventListener('click', () => {
     document.querySelector('body').classList.toggle('dark')
     document.querySelector('#darkmode-switch').classList.toggle('dark')
+
 })
